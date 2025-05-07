d2b4caff83a678d0f7335858fd461da5e37b7b1a
Adiliaguerrero <guerreroadilia37@gmail.com>
2025-05-07 00:02:19 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
+
+
+
 using System.Collections;
-//defincion e inicialización de los arreglos en paralelo y el diccionario
-String[] Departamento = {"Boaco","Carazo","Chinandega","Chontales","Costa Caribe Norte","Costa Caribe Sur","Estelf","Granada","Jinotega","León","Madriz","Managua","Masaya","Matagalpa","Nueva Segovia","Rio San、Juan","Rivas"}; 
-int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744, 1575819, 391903, 593503, 271581, 135446, 182645};
+String[] Departamento = { "Boaco", "Carazo", "Chinandega", "Chontales", "Costa Caribe Norte", "Costa Caribe Sur", "Estelí", "Granada", "Jinotega", "León", "Madriz", "Managua", "Masaya", "Matagalpa", "Nueva Segovia", "Río San Juan", "Rivas" };
+int[] Poblacion = { 185013, 197139, 439906, 190863, 550856, 414543, 229866, 214137, 475630, 421050, 174744, 1546939, 391803, 593503, 271581, 135446, 182645 };
 Dictionary<string, int> diccionario = Departamento
-.Zip(Poblacion, (k, v)=> new {Clave = k, Valor = v})
-.ToDictionary (x => x.Clave, x => x.Valor);
-//fijando los nombres de los departamen tos con menor y mayor poblacion
-string minDepkey = ordenado. First () .Key;
-string maxDepkey= ordenado. Last ().Key;
- //reasignación de los arreglos en paralelo 
-//Mostrar el diccionario sin ordenar
-Console.WriteLine($"datos Desordenados");
+    .Zip(Poblacion, (k, v) => new { Clave = k, Valor = v })
+    .ToDictionary(x => x.Clave, x => x.Valor);
+// Encontrar el mayor y el menor (población)
+int maxDep = Poblacion.Max();
+int minDep = Poblacion.Min();
+string maxDepkey = "", minDepkey = "";
+// Encontrar el nombre del departamento que tiene la mayor y menor población
+for (var i = 0; i < Poblacion.Length; i++)
+{
+    if (Poblacion[i] == maxDep)
+        maxDepkey = Departamento[i];
+    if (Poblacion[i] == minDep)
+        minDepkey = Departamento[i];
+}
+// Mostrar el diccionario sin ordenar
+Console.WriteLine($"Datos desordenados:");
 foreach (var item in diccionario)
-Console.WriteLine ($"{item.Key,-20} ==> {item.Value,10:N0}");
- //Sumar todas las poblacione con SUM de LINQ 
-Console.WriteLine ($" Población General:{diccionario.Values.Sum():N0}");
-Console.WriteLine ($"Departamento con mayor Población:{maxDepkey}");
-Console.WriteLine ($"Departamento con menor Población:{minDepkey}");
+    Console.WriteLine($"({item.Key,-20}) == {item.Value,10:N0}");
+    Console.WriteLine();
+// Mostrar resultado
+for (var i = 0; i < Poblacion.Length; i++)
+    Console.WriteLine($"{Departamento[i],20} => {Poblacion[i],10:N0}");
+// Sumar todas las poblaciones con SUM de LINQ
+Console.WriteLine($"Población General: {diccionario.Values.Sum():N0}");
+Console.WriteLine($"Departamento con mayor población: {maxDepkey}");
+Console.WriteLine($"Departamento con menor población: {minDepkey}");
+
